1a1c2d422303bdc2ed83a998b891c304ec07623d
Daniel Murphy <rdanielmurphy@gmail.com>
2015-10-15 01:53:53 -0400

    Updated credits and removed states/terrotories that crash the app.

--- app/src/main/java/app/sbaloan/utils/SBALoanConstants.java
+++ app/src/main/java/app/sbaloan/utils/SBALoanConstants.java
@@ -14,9 +14,9 @@ import app.sbaloan.models.TypeSearchDto;
 public class SBALoanConstants {
 	public static final List<String> VALUES = Arrays.asList("Don't Care", "Yes", "No");
 
-    public static final List<String> STATES = Arrays.asList("AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL",
-            "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MH", "MA", "MI", "FM", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP",
-            "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY");
+    public static final List<String> STATES = Arrays.asList("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL",
+            "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
+            "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "VI", "WA", "WV", "WI", "WY");
 
 	public static final List<String> TYPES = Arrays.asList("Grant", "Loan", "Venture Capital", "Tax Incentive");
 
